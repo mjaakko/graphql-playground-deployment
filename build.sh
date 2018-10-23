7c8789464ac1345719faf3a16fc0b49bd6af7a2e
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2018-10-23 16:38:27 +0300

    Fix build script

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 set -e
 
 cd graphql-playground/packages/graphql-playground-react
+git submodule update --init
 yarn
 yarn build
 cd ../../..
 
-rm -r dist
+rm -rf dist
 mkdir -p dist/static/css
 mkdir -p dist/static/js
 cp graphql-playground/packages/graphql-playground-react/build/static/css/index.css dist/static/css/index.css
