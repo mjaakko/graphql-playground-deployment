0a94ebac2b9a840507542927aabdee4f31ed2a69
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2018-11-09 15:32:43 +0200

    Add support for custom endpoints

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ const html = fs.readFileSync('index.html').toString()
 
 const app = express();
 
+const getDigitransitUrl = (config) => {
+  return "https://api.digitransit.fi/routing/v1/routers/" + config + "/index/graphql";
+}
+
+const url = (config) => (process.env.ENDPOINT || getDigitransitUrl(config));
+
 app.use(express.static('dist'));
 
+app.use('/graphql-playground/:config', (req, res) => {
+  res.send(html.replace('__GRAPHQL__API__ENDPOINT__', url(req.params.config)));
+});
+
 app.use('/graphql-playground', (req, res) => {
-  res.send(html.replace('__GRAPHQL__API__ENDPOINT__', 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'));
+  res.redirect('/graphql-playground/hsl');
 });
 
 app.listen(port, () => console.log('Started'));
